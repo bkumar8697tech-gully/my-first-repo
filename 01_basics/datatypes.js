5b7514c5e26fb005b91426ab985a675b9d7f809b
bkumar8697tech-gully <54615650+bkumar8697tech-gully@users.noreply.github.com>
2024-10-11 03:20:53 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+//Primitive data types
+//number->2^53
+//bigint
+//string->""
+//boolean->true/false
+//null->standalone value of empty 
+//undefined-> value not assigned not now
+//symbol->unique 
+
+//non-primitive data types
+//object
+
+let name="Brijesh"
+let id=1001
+let rank=12345678902
+b=true
+const rollnum=9987456
+console.log(typeof(name));
+console.log(typeof(id));
+console.log(typeof(rank));
+console.log(typeof(b));
+console.log(typeof(rollnum));
+
+
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
+
