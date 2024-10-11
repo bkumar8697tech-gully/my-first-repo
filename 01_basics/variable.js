bfac16dc7089178750283a86c332c35e0defa645
bkumar8697tech-gully <54615650+bkumar8697tech-gully@users.noreply.github.com>
2024-10-11 07:36:32 +0530

    container config done

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,1 +1,11 @@
+const accountId=901
+let accountEmail="bkd@google.com"
+var accountPassword="Bk12323@"
+accountCity="Jaipur"
 
+/* prefer not to use var
+because of issue in block scope and functional scope */
+
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
