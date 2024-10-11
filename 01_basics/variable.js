fa9b5f5248e8b64dd9521e6c2f5c37a5430ae4a4
bkumar8697tech-gully <54615650+bkumar8697tech-gully@users.noreply.github.com>
2024-10-11 03:21:44 +0000

    container config done

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,10 +2,29 @@ const accountId=901
 let accountEmail="bkd@google.com"
 var accountPassword="Bk12323@"
 accountCity="Jaipur"
-
+let s;
 /* prefer not to use var
 because of issue in block scope and functional scope */
 
 console.log(accountCity);
 
 console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+//Primitive data types
+//number->2^53
+//bigint
+//string->""
+//boolean->true/false
+//null->standalone value of empty 
+//undefined-> value not assigned not now
+//symbol->unique 
+
+//non-primitive data types
+//object
+
+console.log(typeof(accountCity));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
+
