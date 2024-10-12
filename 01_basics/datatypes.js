9b15ce7d8b4518066b4c6b6995d732b2aa5147f5
bkumar8697tech-gully <54615650+bkumar8697tech-gully@users.noreply.github.com>
2024-10-12 13:33:30 +0000

    container config done

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -25,6 +25,63 @@ console.log(typeof(rollnum));
 
 console.log(typeof(null));
 console.log(typeof(undefined));
+//JavaScript is a dynamically because here we don't need to declare
+// data types... 
+//Primitive
+
+//7 types: String, Number, Boolean, null, undefined, Symbol(to make unique), BigInt
+
+const studentName="Brijesh Kumar"
+const score=100
+const scoreValue=100.23
+
+const isLoggedIn = false
+const outsideTemp = null;
+let userEmail;
+
+const id1=Symbol('123')
+const anotherId= Symbol('123')
+
+const bigNumber=7895456252546565n
+console.log(id1===anotherId);
+
+console.log(typeof studentName);
+console.log(typeof score, scoreValue);
+console.log(typeof id1);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof bigNumber)
+
+
+
+//Non- Primitive or Reference
+
+// Array, Objects, Functions
+
+const hero=["Shaktimaan","naagraj","doga"]
+
+let myObj={
+    name:"Brijesh",
+    age:28,
+    admission:true
+}
+
+const myFunction=function(){
+    console.log("Hello World...");
+    
+}
+
+(function(){
+    "use strict";
+
+    function greetMe(yourName){
+        alert('Hello ${yourName}');
+    }
+
+    greetMe("World");
+
+});
 
 
 
